150d40f04b84f4aaa49c23be8c369e5f81a53fd4
Martin <martin.grotz@gmx.de>
2015-09-26 13:19:09 +0200

    Extension Method Klasse eingefügt

---
+++ Base/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace IwAutoUpdater.CrossCutting.Base
+{
+    public static class ExtensionMethods
+    {
+        public static string ReplaceIgnoreCase(this string str, string oldText, string newText)
+        {
+            return Regex.Replace(str,
+                Regex.Escape(oldText),
+                Regex.Replace(newText, "\\$[0-9]+", @"$$$0"),
+                RegexOptions.IgnoreCase);
+        }
+    }
+}
